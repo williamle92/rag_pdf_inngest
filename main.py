1720814975144c67a6f6333d56f871a2a8bba524
William Le <williamkle92@gmail.com>
2026-01-02 00:20:34 -0800

    feat: write function to ingest and embed pdf file, then store to vector db

--- main.py
+++ main.py
@@ -7,6 +7,14 @@ import os
 import datetime
 
 from inngest.fast_api import serve
+from load_data import load_and_chunk_pdf, embed_texts
+from vector_db import QdrantStorage
+from serializer import (
+    RAGChunkAndSource,
+    RAGUpsertPayload,
+    RAGSearchResult,
+    RAGQueryResult,
+)
 
 load_dotenv()
 
@@ -17,12 +25,51 @@ inggest_client: inngest.Inngest = inngest.Inngest(
     serializer=inngest.PydanticSerializer(),
 )
 
+
 @inggest_client.create_function(
     fn_id="Ingest PDF",
     trigger=inngest.TriggerEvent(event="ingest_pdf"),
 )
-async def ingest_pdf(ctx: inngest.Context) -> None: 
-    return {"hello": "world"}
+async def ingest_pdf(ctx: inngest.Context) -> None:
+    def _load(ctx: inngest.Context) -> RAGChunkAndSource:
+        path: str = ctx.event.data.get("file_path", "")
+        if not path:
+            raise ValueError("file_path is required")
+
+        source_id: str = ctx.event.data.get("source_id", None)
+        chunks: list[str] = load_and_chunk_pdf(file_path=path)
+        if not chunks:
+            raise ValueError("No chunks were loaded from the provided PDF file.")
+        return RAGChunkAndSource(chunks=chunks, source_id=source_id)
+
+    def _upsert(chunks_and_src: RAGChunkAndSource) -> RAGUpsertPayload:
+        chunks = chunks_and_src.chunks
+        source_id = chunks_and_src.source_id
+
+        vectors = embed_texts(chunks)
+        ids = [
+            str(uuid.uuid5(uuid.NAMESPACE_URL, f"{source_id}:{i}"))
+            for i in range(len(chunks))
+        ]
+        payloads = [
+            {"source": source_id, "text": chunks[i]} for i in range(len(chunks))
+        ]
+        QdrantStorage().upsert(ids=ids, vectors=vectors, payloads=payloads)
+        
+        return RAGUpsertPayload(ingested=len(chunks))
+
+    chunks_and_source = await ctx.step.run(
+        step_id="load-and-chunk",
+        handler=lambda: _load(ctx),
+        output_type=RAGChunkAndSource,
+    )
+    ingested: RAGUpsertPayload = await ctx.step.run(
+        step_id=("embed-and-upsert"),
+        handler=lambda: _upsert(chunks_and_source),
+        output_type=RAGUpsertPayload,
+    )
+
+    return ingested.model_dump()
 
 
 app: FastAPI = FastAPI()
