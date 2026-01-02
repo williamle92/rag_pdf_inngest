084b05f867650f888f4ad356d79d1a1d98e02cd0
William Le <williamkle92@gmail.com>
2026-01-01 23:34:11 -0800

    feat: use pydantic models to serialize results and input

---
+++ serializer.py
@@ -0,0 +1,21 @@
+from pydantic import BaseModel
+
+
+class RAGChunkAndSource(BaseModel):
+    chunks: list[str]
+    source: str | None = None
+
+
+class RAGUpsertPayload(BaseModel):
+    ingested: int
+
+
+class RAGSearchResult(BaseModel):
+    contexts: list[str]
+    sources: list[str]
+
+
+class RAGQueryResult(BaseModel):
+    answer: str
+    sources: list[str]
+    number_contexts: int
